8f020e5d2ba98448f071aa52e77a94d8e2126a65
frankzengjj <tianyuzeng1997@gmail.com>
2019-07-18 19:34:20 -0500

    add front end

---
+++ src/main/resources/static/precache-manifest.a72ee82ee3822872d88c2092c82ce5d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717fa99d23e71ead1af948265c066c3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8175f4c1e81e32ba1375",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f73ba1f064e8800ce421",
+    "url": "/static/css/main.d3c1742d.chunk.css"
+  },
+  {
+    "revision": "8175f4c1e81e32ba1375",
+    "url": "/static/js/2.9ecd4ef2.chunk.js"
+  },
+  {
+    "revision": "f73ba1f064e8800ce421",
+    "url": "/static/js/main.e61abb91.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
